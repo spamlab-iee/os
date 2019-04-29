e0d27bc8d7344d41b8e315d38d63c924f82c49e8
Arthur Endlein <endarthur@gmail.com>
2019-04-29 15:19:50 -0300

    travis testing take 2

---
+++ install_osx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#from mu editor https://github.com/mu-editor/mu/blob/master/package/install_osx.sh
+set -ev
+brew update >/dev/null 2>&1  # This produces a lot of output that's not very interesting
+
+# Install Python 3.6
+#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+brew upgrade pyenv 
+# The following are needed for Matplotlib
+brew install freetype
